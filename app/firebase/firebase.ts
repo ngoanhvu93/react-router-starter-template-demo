10c2b58471db792b9d0407929febb6ce7b322b40
Vu Ngo <ngoanhvu110293@gmail.com>
2025-07-29 22:32:47 +0700

    Add react-toastify dependency and refactor UserProfile component for improved authentication handling. Initialize Firebase services including Firestore, Storage, and Realtime Database.

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +21,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const rtdb = getDatabase(app);
+// Only initialize analytics on the client side
+export const analytics =
+  typeof window !== "undefined" ? getAnalytics(app) : null;
